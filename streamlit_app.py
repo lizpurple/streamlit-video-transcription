7ca1d4f61937ada865e0f3b88d12db27078db085
lizpurple <m_cteles@hotmail.com>
2025-01-19 15:49:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,13 @@ def extract_video_url(page_url):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        )
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        chrome_options.add_argument("accept-language=en-US,en;q=0.9")
+        chrome_options.add_argument("accept-encoding=gzip, deflate, br")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("enable-automation")
+        chrome_options.add_argument("--remote-debugging-port=9222")
 
         # Start WebDriver
         logging.info("Starting WebDriver...")

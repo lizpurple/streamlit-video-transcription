98a84ad66ea1ca2f1ab995fba0e38b8a70ba767f
lizpurple <m_cteles@hotmail.com>
2025-01-19 15:36:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,15 @@ def extract_video_url(page_url):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        )
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("start-maximized")
 
         # Start WebDriver
-    service = Service()  # chromedriver_autoinstaller ensures the path is correct
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    return driver
+        service = Service()  # chromedriver_autoinstaller ensures the path is correct
+        driver = webdriver.Chrome(service=service, options=chrome_options)
 
         driver.get(page_url)
         time.sleep(10)  # Allow time for JavaScript to load

18a2c8c0aee90ad7d7844ea1e7cd7c6cfbc08451
lizpurple <m_cteles@hotmail.com>
2025-01-19 23:47:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_driver():
 driver = get_driver()
 
 # Access the webpage
-driver.get("https://www.jw.org")
+driver.get("https://www.wikipedia.org")
 time.sleep(5)  # Wait for the page to load
 
 # Take a screenshot and save it

708b885b08f7c0dd63ab5ea5fcf0ed71ed8707e4
lizpurple <m_cteles@hotmail.com>
2025-02-16 20:55:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("http://www.jw.org/pt")
+    driver.get("http://wol.jw.org")
 
     st.code(driver.page_source)
